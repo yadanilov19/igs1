83572c421b55febe8c1b5ff8b4300b27bd7df860
yadanilov19 <yadanilov@gmail.com>
2018-03-28 00:40:05 +0500

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {KonvaModule} from 'ng2-konva';
+import { DrawPlaceComponent } from './components/draw-place/draw-place.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawPlaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KonvaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
