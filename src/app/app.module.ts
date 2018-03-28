6583f280175325df1de91d7505667e4f9c3d6619
yadanilov19 <yadanilov@gmail.com>
2018-03-28 19:13:44 +0500

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {KonvaModule} from 'ng2-konva';
 import { DrawPlaceComponent } from './components/draw-place/draw-place.component';
-
+import {RouterModule, Routes} from '@angular/router';
+import { ControlsComponent } from './components/controls/controls.component';
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    DrawPlaceComponent
+    DrawPlaceComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
-    KonvaModule
+    KonvaModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
